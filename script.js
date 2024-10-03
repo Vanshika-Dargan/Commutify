f04b47903d3eca36900dabdd7d72b796120c0615
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-03 17:33:53 +0530

    drawing route from source to destination

--- script.js
+++ script.js
@@ -3,20 +3,25 @@
 let map = L.map('map').setView([30.1158, 78.2853],13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    maxZoom: 20,
+    maxZoom: 16,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
 
-navigator.geolocation.getCurrentPosition(onLocationSuccess,onLocationError);
+navigator.geolocation.getCurrentPosition(onLocationSuccess,onLocationError,{
+    enableHighAccuracy: true,
+    timeout: 10000, 
+    maximumAge: 0 
+});
 
 function onLocationSuccess(position){
     const userCoordinates=[position.coords.latitude,position.coords.longitude];
+    console.log(userCoordinates);
     
     L.marker(userCoordinates).addTo(map).bindPopup('You are here').openPopup();
 
-    let riderCoordinates = [position.coords.latitude + 0.01, position.coords.longitude + 0.01];
+    let riderCoordinates = [30.104867698463696, 78.29907060455753];
 
     L.marker(riderCoordinates).addTo(map).bindPopup('Rider is here..').openPopup();
     const bounds = L.latLngBounds([userCoordinates, riderCoordinates]);
@@ -29,7 +34,20 @@ function onLocationError(error){
     alert('Unable to retrieve your location'+error.message)
 }
 
+// function drawRoute(userCoordinates,riderCoordinates){
+// const latlngs=[userCoordinates,riderCoordinates];
+// const polyline = L.polyline(latlngs,{color:'red'}).addTo(map);
+// }
+
 function drawRoute(userCoordinates,riderCoordinates){
-const latlngs=[userCoordinates,riderCoordinates];
-const polyline = L.polyline(latlngs,{color:'red'}).addTo(map);
+    L.Routing.control({
+        waypoints:[
+            L.latLng(userCoordinates),
+            L.latLng(riderCoordinates)
+        ],
+        routeWhileDragging: true,
+        lineOptions:{
+            styles:[{color:'purple',weight: 4}]
+        }
+    }).addTo(map);
 }
