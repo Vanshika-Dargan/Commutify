94218fdd936f85fa296f10cc2700c9e70023d287
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-03 15:13:39 +0530

    intial setup got user location and added polyline till the rider location

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// lat long zoom level
+let map = L.map('map').setView([30.1158, 78.2853],13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+navigator.geolocation.getCurrentPosition(onLocationSuccess,onLocationError);
+
+function onLocationSuccess(position){
+    const userCoordinates=[position.coords.latitude,position.coords.longitude];
+    
+    L.marker(userCoordinates).addTo(map).bindPopup('You are here').openPopup();
+
+    let riderCoordinates = [position.coords.latitude + 0.01, position.coords.longitude + 0.01];
+
+    L.marker(riderCoordinates).addTo(map).bindPopup('Rider is here..').openPopup();
+    const bounds = L.latLngBounds([userCoordinates, riderCoordinates]);
+    console.log(bounds);
+    map.fitBounds(bounds);
+    drawRoute(userCoordinates,riderCoordinates);
+}
+
+function onLocationError(error){
+    alert('Unable to retrieve your location'+error.message)
+}
+
+function drawRoute(userCoordinates,riderCoordinates){
+const latlngs=[userCoordinates,riderCoordinates];
+const polyline = L.polyline(latlngs,{color:'red'}).addTo(map);
+}
